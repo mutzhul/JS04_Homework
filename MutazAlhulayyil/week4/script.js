d8ed7df1b9cbb353f56a68c6895956a55db8812f
mutzhul <52379524+mutzhul@users.noreply.github.com>
2019-11-05 10:11:20 +0300

    HW 4
    Completeness: 5/5
    Comfort: 5/5
    
    Note: Thanks!

---
+++ MutazAlhulayyil/week4/script.js
@@ -0,0 +1,40 @@
+console.log("JS connected"); // this to check that our script is connected to out HTML
+
+var body = document.getElementsByTagName('body')[0].style.color= "green";
+;
+
+body = document.getElementsByTagName('body')[0].style.fontFamily = "Arial, sans-serif";
+
+body = document.getElementsByTagName('body')[0].style.backgroundColor = "gray";
+
+
+
+// query select body 
+// change the font family of the body to 'Arial, sans-serif' Hint: use .style to acess CSS properties
+// change the backgroundColor to a color of your choice
+
+var nickname =  document.getElementById("nickname").innerHTML = "Wowza!";
+
+
+
+// qurey select the ID nickname
+//add some value to nickname Hint: innerHTML
+
+var favorites =  document.getElementById("favorites").innerHTML = "Pizza!";
+var homeTown =  document.getElementById("hometown").innerHTML = "Riyadh!";
+
+// query select favorites byID
+// query select hometown byID
+// feed both favorites and homwtown with values that represents you i.e hometown.innerHTML = 'Riyadh';
+
+
+
+//change the color of all selectors to colors of your choice, it better be good LOL
+
+// OK here's one
+body = document.getElementsByTagName('body')[0].style.color= "#00b140";
+
+
+
+
+// https://www.w3schools.com/jsref/prop_style_color.asp
